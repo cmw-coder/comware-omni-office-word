4a9ceb92cec5f788b9efc64b9576333b793d544a
ParticleG <particle_g@outlook.com>
2024-12-19 16:20:33 +0800

    - Init workflow - Change port

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -8,7 +8,7 @@ declare module 'vue' {
   }
 }
 
-export const rawModelApi = axios.create({ baseURL: 'http://10.113.36.127:9190' })
+export const rawModelApi = axios.create({ baseURL: 'http://10.113.36.113:9190' })
 
 export default defineBoot(({ app }) => {
   app.config.globalProperties.$rawModelApi = rawModelApi
